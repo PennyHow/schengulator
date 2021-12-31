db31d1ff73127668664d4c6a7834e80388426283
PennyHow <pho@geus.dk>
2021-12-31 13:40:42 +0100

    dist update

--- schengulator/schengulator.py
+++ schengulator/schengulator.py
@@ -1,16 +1,6 @@
 """
 Schengulator is a tool to calculate how many days an individual has been in
-Schengen countries out of a specified 180-day period. The Schengulator 
-determines the number of days spent and remaining based on the 90/180 Schengen 
-Visa Rule:
-    
-"The 180-day period keeps rolling. Therefore, anytime you wish to enter the 
-Schengen, you just have to count backwards the last 180 days, and see if you 
-have been present in the Schengen for more than 90 days throughout that period"
-
-This tool was inspired by these related python repositories:
-- schengen: https://github.com/weddige/schengen
-- schengencalc: https://github.com/nuno-filipe/schengencalc
+Schengen countries out of a specified 180-day period.
 
 @author: Penelope How
 """
